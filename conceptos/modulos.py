dba953ed665933e36b500bad4971b1e01270939b
wilberth-cl <wilberthfrancisco.cl@gmail.com>
2023-03-14 17:25:48 -0600

    ADD: use modules --Conceptos

---
+++ conceptos/modulos.py
@@ -0,0 +1,18 @@
+### IMPORTACION DE MODULOS DE PYTHON
+
+# import datetime
+# print( datetime.date.today() )
+# print( datetime.timedelta(minutes=100) )
+
+# from datetime import timedelta
+# print( timedelta(minutes=100) )
+
+### IMPORTACION DE MODULOS PROPIOS
+
+import mimodulofmath
+
+a = mimodulofmath.add(1,2)
+print(a)
+
+b = mimodulofmath.substract(1,4)
+print(b)
